228e8f404098964ffec03ae6ac10df1003a59851
Ariel Videla <ariel_videla@hotmail.com>
2023-08-16 23:01:45 -0300

    cuarto commit

--- frontend/src/app/components/detail/detail.component.ts
+++ frontend/src/app/components/detail/detail.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { Product } from '../../interfaces/products';
 
 @Component({
   selector: 'app-detail',
